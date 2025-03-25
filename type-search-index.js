0a5118b4333ac382f29e6705e289380253c3de96
BrutusGamesTeam <oficiallazarentos@gmail.com>
2025-03-24 22:42:03 -0300

    First commit for this repository.

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.scpre.runtime.engine","l":"AnimationType"},{"p":"org.scpre.runtime.engine","l":"Engine"},{"p":"org.scpre.runtime.engine","l":"IEngine"}];updateSearchResults();
